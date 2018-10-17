a668c987da7a682eb69e5beb83088f194643a7cc
Aiden71601 <37307697+Aiden71601@users.noreply.github.com>
2018-10-17 10:14:44 -0400

    Added Files

---
+++ BoxName.java
@@ -0,0 +1,9 @@
+public class BoxName
+{
+	public static void main(String[] args)
+	{
+		System.out.print("+-------+\n");
+		System.out.print("| Aiden |\n");
+		System.out.print("+-------+\n");
+	}
+}
---
+++ MorseCode.java
@@ -0,0 +1,11 @@
+public class MorseCode
+{
+	public static void main(String[] args)
+	{
+		System.out.print("._");
+		System.out.print(" ..");
+		System.out.print(" _..");
+		System.out.print(" .");
+		System.out.print(" _.\n");
+	}
+}
